221d6ebfb45377c14c7fea93aec4bc52ad12aea6
toloh <34540587+toloh@users.noreply.github.com>
2021-01-09 20:29:14 +0000

    Deploying to gh-pages from master @ 512360d4107f77a33216b2591346882563276304 🚀

---
+++ 16x16extratiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="16x16extratiles" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="../16x16extratiles.PNG" width="112" height="64"/>
+</tileset>
---
+++ tileset-advance (1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileset-advance (1)" tilewidth="16" tileheight="16" tilecount="7984" columns="8">
+ <image source="tileset-advance (1).png" width="128" height="15971"/>
+</tileset>
